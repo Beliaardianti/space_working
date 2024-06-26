bd772ea2f27c3302299240a6694e962f62761029
Belia Ardianti <114715788+Beliaardianti@users.noreply.github.com>
2024-06-26 14:57:25 +0700

    Update card-review.tsx

--- src/components/molecules/card/card-review.tsx
+++ src/components/molecules/card/card-review.tsx
@@ -2,33 +2,28 @@ import Image from 'next/image';
 import CardStar from '@/components/molecules/card/card-star';
 import Title from '@/components/atomics/title';
 
-function CardReview({
-  rating,
-  review,
-  avatar,
-  username,
-  jobdesk
-} : {
-  rating: number
-  review: string
-  avatar: string
-  username: string
-  jobdesk: string
-}) {
+interface CardReviewProps {
+  rating: number;
+  review: string;
+  avatar: string;
+  username: string;
+  jobdesk: string;
+}
+
+const CardReview: React.FC<CardReviewProps> = ({ rating, review, avatar, username, jobdesk }) => {
   return (
     <figure className='bg-gray-light rounded-lg px-4 py-4 space-y-4 md:px-5 md:py-4 md:space-y-5'>
       <CardStar rating={rating} variant='review'/>
       <span className='block text-secondary text-sm md:text-base leading-7 md:leading-8'>
-        { review }
+        {review}
       </span>
       <div className='flex items-center mt-3 md:mt-0 md:space-x-3'>
         <div className='relative h-12 w-12 md:h-16 md:w-16'>
           <Image
             src={avatar}
             alt='avatar'
-            layout='fill'
-            objectFit='cover'
-            className='rounded-full'
+            fill
+            className='rounded-full object-cover'
           />
         </div>
         <div className='flex flex-col'>
@@ -36,12 +31,12 @@ function CardReview({
             title={username}
             subtitle={jobdesk}
             section='header'
-      
+            
           />
         </div>
       </div>
     </figure>
-  )
+  );
 }
 
 export default CardReview;
